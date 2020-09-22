b9d9a51c88d84df63c5dcbabfbfa8dcd7630a761
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-22 16:14:09 -0300

    Exs 1 through 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import About from './About';
+import Users from './Users';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+        <Route path="/about" component={About} />
+        <Route path="/users" component={Users} />
+        <Route exact path="/" component={Home} />
+      </BrowserRouter>
+      
     );
   }
 }
